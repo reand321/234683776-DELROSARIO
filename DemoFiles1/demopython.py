684a88b51acacc7a038b7274f811b95a840ec9db
reand321 <johndelrosario123@gmail.com>
2024-07-23 08:42:17 +0800

    Delete DemoFiles1/demopython.py

--- DemoFiles1/demopython.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def calculate_area(radius):
-    return math.pi * radius * radius
-
-radius = float(input("Enter the radius of the circle: "))
-area = calculate_area(radius)
-print(f"The area of the circle is: {area}")
